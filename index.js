0ca220811ad17e00955663c2686b44a612f0feb1
Kelvin <edeh47245@gmail.com>
2025-08-06 19:20:32 +0100

    Making a camera

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const h = window.innerHeight;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
+
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
