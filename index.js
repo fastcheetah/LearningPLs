6b02733c44c134051854e152120d0743181cf50c
Kelvin <edeh47245@gmail.com>
2025-08-06 19:53:54 +0100

    Adding the light, wiremesh light renderer and animation frame

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+// const camera = new THREE.PerspectiveCamera(field of view, aspect, near, far);
+camera.position.z = 2;
+const scene = new THREE.Scene();
+
+const geo = new THREE.IcosahedronGeometry(1.0,2);
+const mat = new THREE.MeshStandardMaterial({ color: 0xffffff, flatShading: true });
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+const wireMat = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true });
+const wireMesh = new THREE.Mesh(geo, wireMat);
+scene.add(wireMesh);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
+scene.add(hemiLight);
+
+function animate() {
+    requestAnimationFrame(animate);
+    
+    renderer.render(scene, camera);
+}
+animate();
+renderer.render(scene, camera);
